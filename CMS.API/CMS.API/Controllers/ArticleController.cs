98233ec39c22e8b5715fc924979f13246c7de1f0
FernandoRosas <45068569+FernandoRosas@users.noreply.github.com>
2018-11-30 12:43:03 +0100

    Team C | API controllers

---
+++ CMS.API/CMS.API/Controllers/ArticleController.cs
@@ -0,0 +1,110 @@
+﻿using CMS.API.BLL.BLL;
+using CMS.API.BLL.Interfaces;
+using CMS.API.Helpers;
+using CMS.BE.DTO;
+using System;
+using System.Collections.ObjectModel;
+using System.Web.Http;
+
+namespace CMS.API.Controllers
+{
+    [BasicAuthentication]
+    public class ArticleController : ApiController
+    {
+        private IArticleBLL _bll = new ArticleBLL();
+
+        // GET: api/Article/Articles
+        [HttpGet]
+        [Route("api/article/articles")]
+        public IHttpActionResult GetArticles()
+        {
+            return Ok(_bll.GetArticles());
+        }
+
+        // GET: api/Article/ArticleById?articleid=
+        [HttpGet]
+        [Route("api/article/articlebyid")]
+        public IHttpActionResult GetArticleById(int articleid)
+        {
+            var article = _bll.GetArticleById(articleid);
+            if (article == null) return BadRequest();
+            return Ok(article);
+        }
+
+        // POST: api/Article/AddArticle
+        [HttpPost]
+        [Route("api/article/addarticle")]
+        public IHttpActionResult AddArticle([FromBody] ArticleDTO article)
+        {
+            if (string.IsNullOrEmpty(article.Topic) || string.IsNullOrEmpty(article.Status)) return BadRequest();
+            if (_bll.AddArticle(article)) return Ok();
+            return InternalServerError();
+        }
+
+        // PUT: api/Article/EditArticle
+        [HttpPut]
+        [Route("api/article/editarticle")]
+        public IHttpActionResult EditArticle([FromBody] ArticleDTO article)
+        {
+            if (string.IsNullOrEmpty(article.Topic) || string.IsNullOrEmpty(article.Status)) return BadRequest();
+            if (_bll.EditArticle(article)) return Ok();
+            return InternalServerError();
+        }
+
+        // DELETE: api/Article/DeleteArticle?articleid=
+        [HttpDelete]
+        [Route("api/article/deletearticle")]
+        public IHttpActionResult DeleteArticle(int articleid)
+        {
+            if (_bll.DeleteArticle(articleid)) return Ok();
+            return InternalServerError();
+        }
+
+        // GET: api/Submission/Submissions
+        [HttpGet]
+        [Route("api/submission/submissions")]
+        public IHttpActionResult GetSubmissions()
+        {
+            return Ok(_bll.GetSubmissions());
+        }
+
+        // GET: api/Submission/SubmissionById?submissionid=
+        [HttpGet]
+        [Route("api/submission/submissionbyid")]
+        public IHttpActionResult GetSubmissionById(int submissionid)
+        {
+            var submission = _bll.GetSubmissionById(submissionid);
+            if (submission == null) return BadRequest();
+            return Ok(submission);
+        }
+
+        // POST: api/Submission/AddSubmission
+        [HttpPost]
+        [Route("api/submission/addsubmission")]
+        public IHttpActionResult AddSubmission([FromBody] SubmissionDTO submission)
+        {
+            if (submission.ArticleFile != null) return BadRequest();
+            if (_bll.AddSubmission(submission)) return Ok();
+            return InternalServerError();
+        }
+
+        // PUT: api/Submission/EditSubmission
+        [HttpPut]
+        [Route("api/submission/editsubmission")]
+        public IHttpActionResult EditSubmission([FromBody] SubmissionDTO submission)
+        {
+            if (submission.ArticleFile != null) return BadRequest();
+            if (_bll.EditSubmission(submission)) return Ok();
+            return InternalServerError();
+        }
+
+        // DELETE: api/Submission/DeleteSubmission?submissionId=
+        [HttpDelete]
+        [Route("api/submission/deletesubmission")]
+        public IHttpActionResult DeleteSubmission(int submissionid)
+        {
+            if (_bll.DeleteSubmission(submissionid)) return Ok();
+            return InternalServerError();
+        }
+    }
+}
---
+++ CMS.API/CMS.API/Controllers/EmergencyInfoController.cs
@@ -0,0 +1,60 @@
+﻿using CMS.API.BLL.BLL;
+using CMS.API.BLL.Interfaces;
+using CMS.API.Helpers;
+using CMS.BE.DTO;
+using System;
+using System.Web.Http;
+
+namespace CMS.API.Controllers
+{
+    [BasicAuthentication]
+    public class EmergencyInfoController : ApiController
+    {
+        private IEmergencyInfoBLL _bll = new EmergencyInfoBLL();
+
+        // GET: api/EmergencyInfo/EmergencyInfoInfo
+        [HttpGet]
+        [Route("api/emergencyinfo/emergencyinfoinfo")]
+        public IHttpActionResult GetEmergencyInfoInfo()
+        {
+            return Ok(_bll.GetEmergencyInfoInfo());
+        }
+
+        // GET: api/EmergencyInfo/EmergencyInfoById?emergencyid=
+        [HttpGet]
+        [Route("api/emergencyinfo/emergencyinfobyid")]
+        public IHttpActionResult GetEmergencyInfoById(int emergencyid)
+        {
+            var emergency = _bll.GetEmergencyInfoById(emergencyid);
+            if (emergency == null) return BadRequest();
+            return Ok(emergency);
+        }
+
+        // POST: api/EmergencyInfo/AddEmergencyInfo
+        [HttpPost]
+        [Route("api/emergencyinfo/addemergencyinfo")]
+        public IHttpActionResult AddEmergencyInfo([FromBody] EmergencyInfoDTO emergency)
+        {
+            if (_bll.AddEmergencyInfo(emergency)) return Ok();
+            return InternalServerError();
+        }
+
+        // PUT: api/EmergencyInfo/EditEmergencyInfo
+        [HttpPut]
+        [Route("api/emergencyinfo/editemergencyinfo")]
+        public IHttpActionResult EditEmergencyInfo([FromBody] EmergencyInfoDTO emergency)
+        {
+            if (_bll.EditEmergencyInfo(emergency)) return Ok();
+            return InternalServerError();
+        }
+
+        // DELETE: api/EmergencyInfo/DeleteEmergencyInfo?emergencyId=
+        [HttpDelete]
+        [Route("api/emergencyinfo/deletemergencyinfo")]
+        public IHttpActionResult DeleteEmergencyInfo(int emergencyid)
+        {
+            if (_bll.DeleteEmergencyInfo(emergencyid)) return Ok();
+            return InternalServerError();
+        }
+    }
+}
