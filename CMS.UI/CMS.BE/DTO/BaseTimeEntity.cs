41593032bf50143353c67b60023725d91a8d921a
the8651 <the8651@thi.de>
2018-12-11 15:57:17 +0100

    Team A DAL add BaseTimeEntity

---
+++ CMS.UI/CMS.BE/DTO/BaseTimeEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.BE.DTO
+{
+    public class BaseTimeEntity
+    {
+        public System.DateTime BeginDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+
+        public string Title { get; set; }
+
+    }
+}
--- CMS.UI/CMS.BE/DTO/EventDTO.cs
+++ CMS.UI/CMS.BE/DTO/EventDTO.cs
@@ -1,13 +1,12 @@
 ﻿namespace CMS.BE.DTO
 {
-    public class EventDTO
+    public class EventDTO: BaseTimeEntity
     {
         public int EventID { get; set; }
         public int ConferenceId { get; set; }
         public int? RoomId { get; set; }
-        public string Title { get; set; }
+        
         public string Description { get; set; }
-        public System.DateTime BeginDate { get; set; }
-        public System.DateTime EndDate { get; set; }
+        
     }
 }
