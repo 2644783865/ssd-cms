626ea2f1be134075b8896f76b0e4ee3ffb96211b
Max <mam0755@thin.onmicrosoft.com>
2018-12-18 21:09:54 +0100

    Team A | IPresentationBLL updated

--- CMS.API/CMS.API.BLL/Interfaces/IPresentationBLL.cs
+++ CMS.API/CMS.API.BLL/Interfaces/IPresentationBLL.cs
@@ -7,5 +7,11 @@ namespace CMS.API.BLL.Interfaces
         bool AddPresentation(PresentationDTO presentation);
         bool EditPresentation(PresentationDTO presentation);
         bool DeletePresentation(int presentationId);
+        bool EditGradeOfPresentation(int presentationId, int grade);
+        bool DeleteGradeFromPresentation(int presentationId);
+        bool EditSessionOfPresentation(int presentationId, int sessionId);
+        bool EditSpecialSessionOfPresentation(int presentationId, int specialSessionId);
+        bool DeleteSessionFromPresentation(int presentationId);
+        bool DeleteSpecialSessionFromPresentation(int presentationId);
     }
 }
