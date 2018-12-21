1ac291d9a1cab39ae52e9adb7ff5d26a95d7fd2a
Max <mam0755@thin.onmicrosoft.com>
2018-12-21 23:40:47 +0100

    Team A | Message and LastMessageTest completed with errors in code

---
+++ CMS.API/CMS.API.Tests/LastMessageTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CMS.API.BLL.BLL;
+using CMS.API.BLL.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CMS.API.Tests
+{
+    [TestClass]
+    public class LastMessageTest
+    {
+        ILastMessageBLL bll;
+
+        [TestInitialize]
+        public void InitializeBLL()
+        {
+            bll = new LastMessageBLL();
+        }
+        
+        [TestMethod]
+        public void TestGetLastMessageByPairId()
+        {
+            var result = bll.GetLastMessageByPairId(1);
+            Assert.IsNotNull(result);
+        }
+
+
+/* Doesn't work correclty, work implementation in DAL
+        [TestMethod]
+        public void TestGetLastMessageByFirstId()
+        {
+            var result = bll.GetLastMessageByFirstId(1);
+            Assert.IsNotNull(result);
+        }
+*/
+
+
+/*  Doesn't work correclty, work implementation in DAL
+        [TestMethod]
+        public void TestGetLastMessageBySecondId()
+        {
+            var result = bll.GetLastMessageBySecondId(2);
+            Assert.IsNotNull(result);
+        }
+*/
+    }
+} 
--- CMS.API/CMS.API.Tests/MessageTest.cs
+++ CMS.API/CMS.API.Tests/MessageTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using CMS.API.BLL.BLL;
 using CMS.API.BLL.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -14,6 +15,59 @@ namespace CMS.API.Tests
         public void InitializeBLL()
         {
             bll = new MessageBLL();
+        }
+
+
+/* Doesn't work, because a null value is returned although the is a message in the db
+        [TestMethod]
+        public void TestGetMessages()
+        {
+            var result = bll.GetMessages();
+            Assert.AreEqual(1, result.Count());
+        }
+*/
+
+
+/* Doesn't work, because a null value is returned although the is a message in the db
+        [TestMethod]
+        public void TestGetMessagesBySenderId1()
+        {
+            var result = bll.GetMessagesBySenderId(1);
+            Assert.AreEqual(1, result.Count());
+        }
+*/
+
+
+        [TestMethod]
+        public void TestGetMessagesBySenderId2()
+        {
+            var result = bll.GetMessagesBySenderId(1);
+            Assert.IsNull(result);
+        }
+
+
+/* Doesn't work, because a null value is returned although the is a message in the db
+        [TestMethod]
+        public void TestGetMessagesByReceiverId1()
+        {
+            var result = bll.GetMessagesByReceiverId(2);
+            Assert.AreEqual(1, result.Count());
+        }
+*/
+
+
+        [TestMethod]
+        public void TestGetMessagesByReceiverId2()
+        {
+            var result = bll.GetMessagesByReceiverId(1);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void TestGetMessageById()
+        {
+            var result = bll.GetMessageById(4);
+            Assert.IsNotNull(result);
         }
     }
 }
