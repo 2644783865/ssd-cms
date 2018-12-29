c1d3f236b76002143dbb47de1628ffa3fb08b753
Unknown <j.zawierucha97@gmail.com>
2018-12-29 02:22:14 +0100

    Team C | Static UI updated - Accomodation, Travel, Emergency, Welcome Pack

---
+++ CMS.UI/CMS.UI/Windows/WelcomePack/GuestAdd.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace CMS.UI.Windows.WelcomePack
+{
+    /// <summary>
+    /// Interaction logic for GuestAdd.xaml
+    /// </summary>
+    public partial class GuestAdd : MetroWindow
+    {
+        public GuestAdd()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CMS.UI/CMS.UI/Windows/WelcomePack/GuestsList.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace CMS.UI.Windows.WelcomePack
+{
+    /// <summary>
+    /// Interaction logic for GuestsList.xaml
+    /// </summary>
+    public partial class GuestsList : MetroWindow
+    {
+        public GuestsList()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
